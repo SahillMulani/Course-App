1bb125fb4532bf4f433887820109ac621b72c4a8
SahillMulani <smulani239@gmail.com>
2023-08-17 13:54:05 +0530

    update

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const adminAuthentication = (req,res,next) => {
 
 const userAuthentication = (req,res,next) => {
   const { username , password} = req.headers;
-  const admin = USERS.find( a => a.username === username && a.password === password)
+  const admin = USERS.find( u => u.username === username && a.password === password)
   if(admin)
   {
     next();
   }
   else{
-    res.status(403).json({message : 'Admin Authetication Failed'})
+    res.status(403).json({message : 'User Authetication Failed'})
   } 
 }
 
@@ -50,33 +50,46 @@ app.post('/admin/signup', (req, res) => {
 app.post('/admin/login', adminAuthentication, (req, res) => {
   // logic to log in admin\
   res.status(200).json({message : "Admin logged in successfully"})
-
 });
 
 app.post('/admin/courses', adminAuthentication, (req, res) => {
   // logic to create a course
-  const course = req.body;
+const course = req.body;
+course.id = Math.floor(Math.random() * 100);
+COURSES.push(course);
+res.status(201).json({message: 'Course Created Succuessfully'});
 });
 
-app.put('/admin/courses/:courseId', adminAuthentication (req, res) => {
+app.put('/admin/courses/:courseId', adminAuthentication , (req, res) => {
   // logic to edit a course
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.find(f => f.id == courseId);
+  if(course)
+  {
+    Object.assign(course, req.body);
+  }
+  else{
+    res.status(404).json({message : "Course not found"});
+  }
 });
 
 app.get('/admin/courses', adminAuthentication, (req, res) => {
   // logic to get all courses
+  res.json({courses : COURSES })
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
   const user = req.body;
-  const existingUser = USERS.find(a => a.username === USERS.username);
+  const existingUser = USERS.find(u => u.username === user.username);
+  console.log(existingUser);
   if (existingUser)
   {
     res.status(403).json({ message : "User already exists"});
   }
   else {
-    USERS.push(admin);
+    USERS.push(user);
     res.json({message : "User created successfully"});
   }
 });
@@ -88,14 +101,39 @@ app.post('/users/login', userAuthentication, (req, res) => {
 
 app.get('/users/courses', userAuthentication , (req, res) => {
   // logic to list all courses
+  let filteredCourses = [];
+  for (let i = 0; i<COURSES.length; i++) {
+    if (COURSES[i].published) {
+      filteredCourses.push(COURSES[i]);
+    }
+  }
+  res.json({ courses: filteredCourses });
+
 });
 
 app.post('/users/courses/:courseId', userAuthentication , (req, res) => {
   // logic to purchase a course
+  const courseId = Number(req.params.courseId);
+  const course = COURSES.find(c => c.id === courseId && c.published);
+  if (course) {
+    req.user.purchasedCourses.push(courseId);
+    res.json({ message: 'Course purchased successfully' });
+  } else {
+    res.status(404).json({ message: 'Course not found or not available' });
+  }
 });
 
 app.get('/users/purchasedCourses', userAuthentication , (req, res) => {
   // logic to view purchased courses
+  var purchasedCourseIds = req.user.purchasedCourses; [1, 4];
+  var purchasedCourses = [];
+  for (let i = 0; i<COURSES.length; i++) {
+    if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
+      purchasedCourses.push(COURSES[i]);
+    }
+  }
+
+  res.json({ purchasedCourses });
 });
 
 app.listen(3000, () => {
--- test.js
+++ test.js
@@ -10,11 +10,14 @@ let COURSES = [];
 
 app.post('/admin/login', (req, res) => {
     // logic to log in admin\
-    const { username , password} = req.headers;
-    console.log(req.headers.username);
-    console.log("username : "+ username);
-    console.log("password : "+ password);
+    const course = req.body;
+    console.log(course);
+    course.courseid = 123;
+    res.status(201).json(course);
   });
+
+  //app.put('/admin/courses/:courseId')
+
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
